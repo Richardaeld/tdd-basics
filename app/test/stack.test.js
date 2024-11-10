5053941d14a30a92a9f359c4578ee9b32a3a5add
Richard E <richardaeld@gmail.com>
2024-11-10 13:14:10 -0500

    added to first test

--- app/test/stack.test.js
+++ app/test/stack.test.js
@@ -9,7 +9,8 @@ describe('my stack', () => {
    it('is created empty', () => {
       const stack = new Stack();
 
-      expect(stack.top).toBe(-1)
+      expect(stack.top).toBe(-1);
+      expect(stack.items).toEqual({});
    });
    it.todo('can push to top');
    it.todo('can pop off');
