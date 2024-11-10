7e29a6e696eba3fd699cf0ed6242325beac70a7e
Richard E <richardaeld@gmail.com>
2024-11-10 13:11:54 -0500

    created base project and added test folder with first test

---
+++ app/test/stack.test.js
@@ -0,0 +1,16 @@
+class Stack {
+   constructor() {
+      this.top = -1;
+      this.items = {};
+   }
+}
+
+describe('my stack', () => {
+   it('is created empty', () => {
+      const stack = new Stack();
+
+      expect(stack.top).toBe(-1)
+   });
+   it.todo('can push to top');
+   it.todo('can pop off');
+})
