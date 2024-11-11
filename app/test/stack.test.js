131bc54d85090856372843c085a7a507c9bc5ee0
Richard E <richardaeld@gmail.com>
2024-11-10 19:01:26 -0500

    added 2 more tests

--- app/test/stack.test.js
+++ app/test/stack.test.js
@@ -3,15 +3,42 @@ class Stack {
       this.top = -1;
       this.items = {};
    }
+   get peek() {
+      return this.items[this.top];
+   }
+   push(value) {
+      this.top++;
+      this.items[this.top] = value;
+   }
+   pop() {
+      if (this.items.length === 0) throw new Error ('Stack is empty');
+      this.items[this.top] = undefined;
+      this.top--;
+   }
 }
 
 describe('my stack', () => {
-   it('is created empty', () => {
-      const stack = new Stack();
+   let stack;
 
+   beforeEach(() => {
+      stack = new Stack();
+   });
+
+   it('is created empty', () => {
       expect(stack.top).toBe(-1);
       expect(stack.items).toEqual({});
    });
-   it.todo('can push to top');
-   it.todo('can pop off');
+
+   it('can push to top', () => {
+      stack.push('abc');
+      expect(stack.top).toBe(0);
+      expect(stack.peek).toBe('abc')
+   });
+
+   it('can pop off', () => {
+      stack.push('abc');
+      stack.pop();
+      expect(stack.top).toBe(-1);
+      expect(stack.items).toEqual({});
+   });
 })
